34f06fc9b2778b7ac27d1d4493c33fa04e09c3d0
Cyprien LEFEBVRE <MisterCyp59@gmail.com>
2016-04-02 17:09:58 +0200

    ajout log pour le script d'install

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 ﻿#!/usr/bin/bash
+
+
 INSTALLDIR=$(readlink -f $(dirname $0))
 USER=rufy
 GROUP=web
 VIRTUALDIR="venv"
+
+mkdir -p ${INSTALLDIR}/log/
+
+exec 2>${INSTALLDIR}/log/install.log
 
 echo "
 #########################################
@@ -12,7 +18,7 @@ echo "
 
 echo "
 ####################################
-#####     python 2.7              ####
+#####     python 2.7            ####
 ####################################
 "
 aptitude install -y python2.7
@@ -54,17 +60,16 @@ echo "
 ###########################################################################
 "
 cd $INSTALLDIR
-touch ${USER}_SUPERVISOR.conf
+touch config/${USER}_SUPERVISOR.conf
 
 echo "[program:rufy]
 command = ${INSTALLDIR}/gunicorn_start                    ; Command to start app
 user = ${USER}                                                          ; User to run as
 stdout_logfile = ${INSTALLDIR}/log/gunicorn_supervisor.log   ; Where to write log messages
 redirect_stderr = true                                                ; Save stderr in the same log
-environment=LANG=fr_FR.UTF-8,LC_ALL=fr_FR.UTF-8                       ; Set UTF-8 as default encoding" >> ${USER}_SUPERVISOR.conf
+environment=LANG=fr_FR.UTF-8,LC_ALL=fr_FR.UTF-8                       ; Set UTF-8 as default encoding" >> config/${USER}_SUPERVISOR.conf
 
-cp ${USER}_SUPERVISOR.conf /etc/supervisor/conf.d/${USER}_SUPERVISOR.conf
-mkdir -p log/
+cp config/${USER}_SUPERVISOR.conf /etc/supervisor/conf.d/${USER}_SUPERVISOR.conf
 touch log/gunicorn_supervisor.log 
 
 supervisorctl reread
@@ -72,15 +77,25 @@ supervisorctl update
 
 echo "
 ###########################################################################
+#####           Copie de la base de donnée initiale                    ####
+###########################################################################
+"
+
+cp ${INSTALLDIR}/config/db-init.sqlite3 ${INSTALLDIR}/db.sqlite3
+
+echo "
+###########################################################################
 #####     Configuration de Python pour RuFy avec virtualenv            ####
 ###########################################################################
 "
 mkdir -p $VIRTUALDIR
+
 echo "
 ####################################################
 $USER devient propriétaire du dossier $INSTALLDIR :
 ####################################################
 "
+
 chown -R ${USER}:users $INSTALLDIR
 chmod -R g+w $INSTALLDIR
 chmod +x ${INSTALLDIR}/gunicorn_start
